0088a02639bb10f8ccf2ccb91f2bcc6247b9728e
Maurice <mauricewegner@icloud.com>
2021-04-12 01:21:00 +0200

    footer padding fix

--- components/Button/index.vue
+++ components/Button/index.vue
@@ -113,11 +113,6 @@ export default Vue.extend({
   computed: {
     hasTarget() {
       return this.href?.slice(0, 1) === "#";
-    },
-    localeTitle() {
-      if (typeof this.title === "string") {
-        return this.title;
-      }
     }
   },
   mounted() {
